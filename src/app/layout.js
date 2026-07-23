85585aed3ff1d1aab32f0703d2a7948cd8a634e6
Influencertools123 <sociomedia9955@gmail.com>
2026-07-23 17:16:55 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import SiteShell from "@/components/SiteShell";
 
 const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export const metadata = {
   metadataBase: new URL(SITE_URL),
@@ -40,7 +41,34 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {GTM_ID ? (
+          // Google Tag Manager
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
+            }}
+          />
+        ) : null}
+      </head>
       <body>
+        {GTM_ID ? (
+          // Google Tag Manager (noscript)
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+              title="Google Tag Manager"
+            />
+          </noscript>
+        ) : null}
+
         <SiteShell>{children}</SiteShell>
       </body>
     </html>
